dac022221046dfc13b35c180ad097d1f5df480da
Yurii Rybachok <drummer2009@ukr.net>
2023-01-28 10:58:51 +0200

    add testimonials to testimonials page

--- src/scenes/Testimonials.jsx
+++ src/scenes/Testimonials.jsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 
 const Testimonials = () => {
     const testimonialStyles = `mx-auto relative max-w-[400px] h-[350px] flex flex-col
-        justify-end pt-16 mt-48 before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2`;
+        justify-end p-16 mt-48 before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2`;
 
     return(
         <section id="testimonials" className="pt-32 pb-16">
@@ -28,6 +28,60 @@ const Testimonials = () => {
                     ut fames odio in at. At magna ornare dictum lectus.
                 </p>
             </motion.div>
+            {/* TESTIMONIALS */}
+            <div className="md:flex md:justify-between gap-8">
+                <motion.div
+                    className={`bg-blue ${testimonialStyles} before:content-person1`}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, scale: 0.8 },
+                        visible: { opacity: 1, scale: 1 },
+                    }}
+                >
+                    <p className="font-playfair text-6xl">“</p>
+                    <p className="text-center text-xl">
+                        A auctor pharetra hendrerit mattis amet etiam interdum platea.
+                    </p>
+                </motion.div>
+
+                <motion.div
+                    className={`bg-red ${testimonialStyles} before:content-person2`}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.2, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, scale: 0.8 },
+                        visible: { opacity: 1, scale: 1 },
+                    }}
+                >
+                    <p className="font-playfair text-6xl">“</p>
+                    <p className="text-center text-xl">
+                        Aliquam aliquet integer ut fames odio in at. At magna ornare dictum
+                        lectus.
+                    </p>
+                </motion.div>
+
+                <motion.div
+                    className={`bg-yellow ${testimonialStyles} before:content-person3`}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 0.4, duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, scale: 0.8 },
+                        visible: { opacity: 1, scale: 1 },
+                    }}
+                >
+                    <p className="font-playfair text-6xl">“</p>
+                    <p className="text-center text-xl">
+                        Fames odio in at. At magna ornare dictum lectus.
+                    </p>
+                </motion.div>
+            </div>
         </section>
     )
 }
